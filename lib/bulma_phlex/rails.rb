430034080209f3935a1cf5f7d7399f2dd43e357c
Todd Kummer <toddkummer@users.noreply.github.com>
2025-12-12 13:18:32 -0800

    Define namespace prior to zeitwerk (#8)

--- lib/bulma_phlex/rails.rb
+++ lib/bulma_phlex/rails.rb
@@ -5,19 +5,18 @@ require "bulma-phlex"
 require "phlex-rails"
 require "zeitwerk"
 
+module BulmaPhlex
+  # The Rails namespace provides Bulma Phlex components and helpers for integration with Ruby on Rails applications.
+  module Rails
+  end
+end
+
 loader = Zeitwerk::Loader.for_gem_extension(BulmaPhlex)
 loader.collapse("#{__dir__}/rails/components")
 loader.collapse("#{__dir__}/rails/helpers")
 loader.setup
 
 require "bulma_phlex/rails/engine"
-
-module BulmaPhlex
-  # The Rails namespace provides Bulma Phlex components and helpers for integration with Ruby on Rails applications.
-  module Rails
-    # Your code goes here...
-  end
-end
 
 # Rails-specific extensions to BulmaPhlex components
 BulmaPhlex::Card.include(BulmaPhlex::Rails::CardHelper) if defined?(Turbo)
